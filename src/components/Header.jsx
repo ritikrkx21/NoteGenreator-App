9b05a109e46b7932086392f2c167af33ca6bdbec
KundlasRitik <ritikkundlas@gmail.com>
2022-04-11 02:59:25 +0530

    setting up repo

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
+function Header() {
+  return (
+    <header>
+      <h1><LocalLibraryIcon fontSize="medium"/>NoteGenerator</h1>
+    </header>
+  );
+}
+
+export default Header;
